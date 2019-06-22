0158aef6fb600538ef129e83ab055590ea8fe87f
Roel van Uden <r.h.g.v.uden@outlook.com>
2019-06-22 21:35:08 +0200

    Adds /remote/popular API for popular content

--- src/controllers/RemoteController.ts
+++ src/controllers/RemoteController.ts
@@ -1,17 +1,28 @@
 import * as api from 'express-openapi-json';
 import * as app from '..';
 
-// TODO: Add pagination indicators (hasMorePages?).
 export class RemoteController {
-  @api.createOperation('RemoteSearch')
-  async searchAsync(model: app.IRemoteSearchContext): Promise<api.Result<app.IRemoteSearch>> {
-    const title = model.query.title;
-    const pageNumber = model.query.pageNumber;
+  // TODO: Add pagination indicators (hasMorePages?).
+  @api.createOperation('RemotePopular')
+  async popularAsync(model: app.IRemotePopularContext): Promise<api.Result<app.IRemotePopularResponse>> {
     switch (model.query.providerName) {
       case 'batoto':
-        return api.json(await app.batotoProvider.searchAsync(title, pageNumber));
+        return api.json(await app.batotoProvider.popularAsync(model.query.pageNumber));
       case 'fanfox':
-        return api.json(await app.fanfoxProvider.searchAsync(title, pageNumber));
+        return api.json(await app.fanfoxProvider.popularAsync(model.query.pageNumber));
+      default:
+        throw new Error();
+    }
+  }
+
+  // TODO: Add pagination indicators (hasMorePages?).
+  @api.createOperation('RemoteSearch')
+  async searchAsync(model: app.IRemoteSearchContext): Promise<api.Result<app.IRemoteSearchResponse>> {
+    switch (model.query.providerName) {
+      case 'batoto':
+        return api.json(await app.batotoProvider.searchAsync(model.query.title, model.query.pageNumber));
+      case 'fanfox':
+        return api.json(await app.fanfoxProvider.searchAsync(model.query.title, model.query.pageNumber));
       default:
         throw new Error();
     }
@@ -19,7 +30,7 @@ export class RemoteController {
 
   // TODO: Add resolved url. Because provider can do redirections, and the library needs to understand that later!
   @api.createOperation('RemoteSeries')
-  async seriesAsync(model: app.IRemoteSeriesContext): Promise<api.Result<app.IRemoteSeries>> {
+  async seriesAsync(model: app.IRemoteSeriesContext): Promise<api.Result<app.IRemoteSeriesResponse>> {
     if (app.batotoProvider.isSupported(model.query.url)) {
       return api.json(await app.batotoProvider.seriesAsync(model.query.url));
     } else if (app.fanfoxProvider.isSupported(model.query.url)) {
@@ -30,11 +41,11 @@ export class RemoteController {
   }
 
   @api.createOperation('RemoteStart')
-  async chapterAsync(model: app.IRemoteStartContext): Promise<api.Result<app.IRemoteStart>> {
+  async startAsync(model: app.IRemoteStartContext): Promise<api.Result<app.IRemoteStartResponse>> {
     if (app.batotoProvider.isSupported(model.query.url)) {
-      return api.json(await app.batotoProvider.chapterAsync(model.query.url));
+      return api.json(await app.batotoProvider.startAsync(model.query.url));
     } else if (app.fanfoxProvider.isSupported(model.query.url)) {
-      return api.json(await app.fanfoxProvider.chapterAsync(model.query.url));
+      return api.json(await app.fanfoxProvider.startAsync(model.query.url));
     } else {
       throw new Error();
     }
