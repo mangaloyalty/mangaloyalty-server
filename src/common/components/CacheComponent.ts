994ae8dc4a20849d46602d8b77e84254158dbdcc
Deathspike <>
2019-06-25 09:03:37 +0200

    Adds caching to each non-session remote end-point

--- src/common/components/CacheComponent.ts
+++ src/common/components/CacheComponent.ts
@@ -1,19 +1,24 @@
-export class CacheComponent<T> {
+export class CacheComponent {
   private readonly _timeout: number;
-  private _cacheData?: Promise<T>;
-  private _cacheTime?: number;
+  private _cacheData: {[key: string]: Promise<any>};
+  private _cacheTime: {[key: string]: number};
 
   constructor(timeout: number) {
+    this._cacheData = {};
+    this._cacheTime = {};
     this._timeout = timeout;
   }
 
-  async getAsync(refreshAsync: () => Promise<T>) {
-    if (!this._cacheData || !this._cacheTime || this._cacheTime < Date.now()) {
-      this._cacheData = refreshAsync();
-      this._cacheTime = Date.now() + this._timeout;
-      return await this._cacheData;
-    } else {
-      return await this._cacheData;
+  async getAsync<T>(key: string, refreshAsync: () => Promise<T>) {
+    try {
+      if (this._cacheData[key] && this._cacheTime[key] && this._cacheTime[key] >= Date.now()) return this._cacheData[key];
+      this._cacheData[key] = refreshAsync();
+      this._cacheTime[key] = Date.now() + this._timeout;
+      return await this._cacheData[key];
+    } catch (error) {
+      delete this._cacheData[key];
+      delete this._cacheTime[key];
+      throw error;
     }
   }
 }
