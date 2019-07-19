928a7499bdad5ef3969917d8d0e8fce1b97cb521
Roel van Uden <r.h.g.v.uden@outlook.com>
2019-07-19 20:20:56 +0200

    Library APIs implementation for detail and list

--- src/controllers/SessionController.ts
+++ src/controllers/SessionController.ts
@@ -9,7 +9,7 @@ export class SessionController {
 
   @api.createOperation('SessionPage')
   async sessionPageAsync(model: app.ISessionPageContext): Promise<api.Result<app.ISessionPageResponse>> {
-    const session = app.core.session.get(model.path.sessionId);
+    const session = app.core.session.get(model.path.id);
     const image = session && await session.getImageAsync(model.query.pageNumber);
     if (image) {
       return api.json({image});
--- src/managers/FileManager.ts
+++ src/managers/FileManager.ts
@@ -4,16 +4,29 @@ import * as path from 'path';
 
 export class FileManager {
   async deleteAsync(relativePath: string) {
-    await fs.remove(path.resolve(app.settings.basePath, relativePath));
+    await fs.remove(path.join(app.settings.basePath, relativePath));
   }
   
+  async readdirAsync(relativePath: string) {
+    const absolutePath = path.join(app.settings.basePath, relativePath);
+    await fs.ensureDir(absolutePath);
+    return await fs.readdir(absolutePath);
+  }
+
   async readJsonAsync<T>(relativePath: string) {
-    return await fs.readJson(path.resolve(app.settings.basePath, relativePath)) as T;
+    return await fs.readJson(path.join(app.settings.basePath, relativePath)) as T;
   }
 
   async writeJsonAsync<T>(relativePath: string, value: T) {
-    const absolutePath = path.resolve(app.settings.basePath, relativePath);
+    const absolutePath = path.join(app.settings.basePath, relativePath);
+    const absolutePathTmp = `${absolutePath}.tmp`;
     await fs.ensureDir(path.dirname(absolutePath));
-    await fs.writeJson(absolutePath, value, {spaces: 2});
+    await fs.writeJson(absolutePathTmp, value, {spaces: 2});
+    try {
+      await fs.move(absolutePathTmp, absolutePath, {overwrite: true});
+    } catch (error) {
+      await fs.remove(absolutePathTmp);
+      throw error;
+    }
   }
 }
