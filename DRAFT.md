2990a7769a0202fe50ec1f9eb1e56a1667b88fd5
Roel van Uden <r.h.g.v.uden@outlook.com>
2019-07-19 20:55:11 +0200

    Library APIs implementation for delete series

--- DRAFT.md
+++ DRAFT.md
@@ -1,33 +1,15 @@
 FILE STRUCTURE:
 
-    /manga/{seriesId}/series.json
-    /manga/{seriesId}/{chapterId}/{pageNumber:000}.json
+    /library/{seriesId}/series.json
+    /library/{seriesId}/{chapterId}/{pageNumber:000}.json
 
 SERIES.JSON:
 
     {
-      id: Guid
-      addedAt: Date                         // when was the series added?
-      lastChapterAddedAt: Date              // when was the last chapter added?
-      lastCheckedAt: Date                   // when was the series checked against online sources?
-      lastPageReadAt: Date                  // when did this user last read a page? happens when `pageNumber` is updated, one should always match `lastPageReadAt`
-
       automation: {
         frequency: Frequency                // at which frequency should the online check be automated?
         storeAll: boolean                   // should new chapters be stored automatically?
       }
-      chapters: {                           // chapters are added from each metadata update, but never removed. So, deleted chapters are visible!
-        [url: string]: { // swap url and id so that there is never a collision ever.
-          addedAt: Date                     // when was this chapter added? one should always match `lastChapterAddedAt`
-          deletedAt?: Date                  // when was this chapter found to have been deleted?
-          pageCount?: number                // set when session is a opened, if ever
-          pageNumber?: number               // which is the last page read?
-          id: Guid                          // generated identifier for this chapter
-          stored: boolean                   // has chapter been made available locally?
-          title: string                     // backup in case chapter gets deleted from metadata
-        }
-      },
-      series: openapi:ISeriesDetail       // providerName can be derived, but do delete the chapters. its so unnecessary.
     }
 
 CHAPTER/PAGE: 
@@ -47,30 +29,11 @@ ENUMERATORS:
 
 API:
 
-GET /library
-  200
-  - return serieslist info by accumulating _series.json and flattening user (image, title, id)
-
-POST /library ({url: string})
-  200
-  - server must check if url already has an entry
-    - if so, add user to list (if not yet added)
-    - otherwise get metadata and create new entry with user
-  - return id
-
 PUT /library
   200
   - server refreshes metadata for ALL series
   - return result AS GET
 
-GET /library/{seriesId}
-  200, 404
-  - return described _series.json info flattened for user (for now, 'admin').
-
-DELETE /library/{seriesId}
-  200, 404
-  - server removes from user list of id, and if last user, deletes series from disk
-  - empty result
 
 PUT /library/{seriesId}
   200, 404
--- src/managers/LibraryManager.ts
+++ src/managers/LibraryManager.ts
@@ -4,17 +4,29 @@ import * as path from 'path';
 // TODO: where will be cache?
 // TODO: where will be syncroot? concurrent access on singular series must be protected
 export class LibraryManager {
-  async addAsync(url: string) {
-    const remoteDetail = await remoteAsync(url);
-    const libraryDetail = createDetail(remoteDetail);
-    synchronize(libraryDetail, remoteDetail);
-    await app.core.file.writeJsonAsync(getPath(libraryDetail.id), libraryDetail);
-    return libraryDetail.id;
+  async createAsync(url: string) {
+    const detail = createDetail(await remoteAsync(url));
+    const seriesPath = path.join(app.settings.libraryCore, detail.id, app.settings.librarySeriesName);
+    await app.core.file.writeJsonAsync(seriesPath, detail);
+    return detail.id;
+  }
+
+  async deleteAsync(id: string) {
+    try {
+      const deletePath = path.join(app.settings.libraryCore, `_${id}`);
+      await app.core.file.moveAsync(path.join(app.settings.libraryCore, id), deletePath);
+      await app.core.file.deleteAsync(deletePath);
+      return true;
+    } catch (error) {
+      if (error && error.code === 'ENOENT') return false;
+      throw error;
+    }
   }
 
   async detailAsync(id: string) {
     try {
-      return await app.core.file.readJsonAsync<app.ILibraryDetail>(getPath(id));
+      const seriesPath = path.join(app.settings.libraryCore, id, app.settings.librarySeriesName);
+      return await app.core.file.readJsonAsync<app.ILibraryDetail>(seriesPath);
     } catch (error) {
       if (error && error.code === 'ENOENT') return undefined;
       throw error;
@@ -23,8 +35,9 @@ export class LibraryManager {
 
   async listAsync(pageNumber?: number) {
     // Initialize the series details.
-    const ids = await app.core.file.readdirAsync(app.settings.library);
-    const details = await Promise.all(ids.map((id) => app.core.file.readJsonAsync<app.ILibraryDetail>(getPath(id))));
+    const ids = await app.core.file.readdirAsync(app.settings.libraryCore);
+    const seriesPaths = ids.filter((id) => !id.startsWith('_')).map((id) => path.join(app.settings.libraryCore, id, app.settings.librarySeriesName));
+    const details = await Promise.all(seriesPaths.map((seriesPath) => app.core.file.readJsonAsync<app.ILibraryDetail>(seriesPath)));
     details.sort((a, b) => (b.lastChapterAddedAt || b.addedAt) - (a.lastChapterAddedAt || a.addedAt));
 
     // Initialize the series view.
@@ -37,13 +50,10 @@ export class LibraryManager {
 }
 
 function createDetail(detail: app.IRemoteDetail): app.ILibraryDetail {
-  return {
-    id: app.createUniqueId(),
-    addedAt: Date.now(),
-    lastSyncAt: Date.now(),
-    chapters: [],
-    series: createDetailSeries(detail)
-  };
+  const id = app.createUniqueId();
+  const result = {id, addedAt: Date.now(), lastSyncAt: Date.now(), chapters: [], series: createDetailSeries(detail)};
+  synchronize(result, detail);
+  return result;
 }
 
 function createDetailSeries(detail: app.IRemoteDetail): app.ILibraryDetailSeries {
@@ -58,10 +68,6 @@ function createDetailSeries(detail: app.IRemoteDetail): app.ILibraryDetailSeries
   };
 }
 
-function getPath(id: string) {
-  return path.join(app.settings.library, id, 'series.json');
-}
-
 async function remoteAsync(url: string) {
   if (app.batotoProvider.isSupported(url)) {
     return await app.batotoProvider.seriesAsync(url);
@@ -73,24 +79,24 @@ async function remoteAsync(url: string) {
 }
 
 // TODO: Clean me up
-function synchronize(db: app.ILibraryDetail, remoteDetail: app.IRemoteDetail) {
+function synchronize(destination: app.ILibraryDetail, source: app.IRemoteDetail) {
   const knownIds: {[id: string]: boolean} = {};
 
-  for (const remoteDetailChapter of remoteDetail.chapters) {
-    const libraryDetailChapter = db.chapters.find((chapter) => chapter.url === remoteDetail.url);
-    if (libraryDetailChapter) {
-      libraryDetailChapter.title = remoteDetailChapter.title;
-      knownIds[libraryDetailChapter.id] = true;
+  for (const sourceChapter of source.chapters) {
+    const destinationChapter = destination.chapters.find((chapter) => chapter.url === source.url);
+    if (destinationChapter) {
+      destinationChapter.title = sourceChapter.title;
+      knownIds[destinationChapter.id] = true;
     } else {
       const id = app.createUniqueId();
-      db.chapters.push({id, addedAt: Date.now(), title: remoteDetailChapter.title, url: remoteDetailChapter.url});
+      destination.chapters.push({id, addedAt: Date.now(), title: sourceChapter.title, url: sourceChapter.url});
       knownIds[id] = true;
     }
   }
 
   // TODO: should I remove non-stored chapters?
-  for (const libraryDetailChapter of db.chapters) {
-    if (knownIds[libraryDetailChapter.id]) continue;
-    if (!libraryDetailChapter.deletedAt) libraryDetailChapter.deletedAt = Date.now();
+  for (const destinationChapter of destination.chapters) {
+    if (knownIds[destinationChapter.id]) continue;
+    if (!destinationChapter.deletedAt) destinationChapter.deletedAt = Date.now();
   }
 }
