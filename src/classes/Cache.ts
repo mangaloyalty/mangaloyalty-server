6fc7c43b86aedec9e374869b368d8af4d30f4a92
Deathspike <>
2019-07-20 12:13:57 +0200

    Rework Cache and Session. Remote session use cache

--- src/classes/Cache.ts
+++ src/classes/Cache.ts
@@ -2,73 +2,97 @@ import * as app from '..';
 import * as path from 'path';
 
 export class Cache {
-  private readonly _currentPath: string;
-  private readonly _currentTimeout: number;
+  private readonly _basePath: string;
+  private readonly _expireTimeout?: number;
   private readonly _timeouts: {[key: string]: NodeJS.Timeout};
   private readonly _values: {[key: string]: Promise<any> | string};
-  private _initPromise?: Promise<void>;
 
-  constructor(name: string, timeout: number) {
-    this._currentPath = path.join(app.settings.cacheCore, name);
-    this._currentTimeout = timeout;
+  constructor(name: string, timeout?: number) {
+    this._basePath = path.join(app.settings.cacheCore, name);
+    this._expireTimeout = timeout;
     this._timeouts = {};
     this._values = {};
   }
 
-  async getAsync<T>(key: string, refreshAsync: () => Promise<T>) {
-    if (!this._values[key]) {
-      await this.initAsync();
-      return await this._createAsync(key, refreshAsync);
-    } else if (typeof this._values[key] !== 'string') {
-      return await this._values[key] as T;
-    } else try {
-      return await app.core.file.readJsonAsync<T>(path.join(this._currentPath, String(this._values[key])));
-    } catch (error) {
-      if (error && error.code === 'ENOENT') return await this._createAsync(key, refreshAsync);
-      throw error;
-    }
+  async addAsync<T>(key: string, value: T) {
+    if (this._values[key]) return false;
+    await this._createAsync(key, value);
+    return true;
   }
 
-  async initAsync() {
-    try {
-      this._initPromise = this._initPromise || app.core.file.deleteAsync(this._currentPath);
-      await this._initPromise;
-    } catch (error) {
-      this._initPromise = undefined;
-      throw error;
-    }
-  }
-
-  private async _createAsync<T>(key: string, refreshAsync: () => Promise<T>) {
-    try {
-      const valuePromise = this._values[key] = refreshAsync();
-      const value = await valuePromise;
-      const id = app.createUniqueId();
-      await app.core.file.writeJsonAsync(path.join(this._currentPath, id), value);
-      this._values[key] = id;
-      this._updateTimeout(key, this._currentTimeout);
-      return value;
-    } catch (error) {
-      delete this._values[key];
-      throw error;
-    }
-  }
-
-
-  private _updateTimeout(key: string, timeout: number) {
-    clearTimeout(this._timeouts[key]);
-    this._timeouts[key] = setTimeout(() => {
-      const id = this._values[key];
+  expire(key: string) {
+    const value = this._values[key];
+    if (typeof value === 'string') {
+      clearTimeout(this._timeouts[key]);
       delete this._timeouts[key];
       delete this._values[key];
-      cleanAsync(path.join(this._currentPath, String(id)));
-    }, timeout);
+      removeWithTraceAsync(path.join(this._basePath, value));
+    } else if (value) {
+      value.then(() => expireWithTrace(this, key));
+    }
+  }
+
+  async getAsync<T>(key: string) {
+    const value = this._values[key];
+    if (!value) {
+      return undefined;
+    } else if (typeof value !== 'string') {
+      return await value as T;
+    } else try {
+      return await app.core.file.readJsonAsync<T>(path.join(this._basePath, value));
+    } catch (error) {
+      if (error && error.code === 'ENOENT') return undefined;
+      throw error;
+    }
+  }
+
+  async getOrAddAsync<T>(key: string, valueFactory: () => Promise<T> | T) {
+    const value = this._values[key];
+    if (!value) {
+      return await this._createAsync(key, valueFactory());
+    } else if (typeof value !== 'string') {
+      return await value as T;
+    } else try {
+      return await app.core.file.readJsonAsync<T>(path.join(this._basePath, value));
+    } catch (error) {
+      if (error && error.code === 'ENOENT') return await this._createAsync(key, valueFactory());
+      throw error;
+    }
+  }
+
+  private async _createAsync<T>(key: string, value: Promise<T> | T) {
+    try {
+      const resultPromise = this._values[key] = Promise.resolve(value);
+      const result = await resultPromise;
+      const id = app.createUniqueId();
+      await app.core.file.writeJsonAsync(path.join(this._basePath, id), result);
+      this._values[key] = id;
+      this._updateTimeout(key);
+      return result;
+    } catch (error) {
+      delete this._values[key];
+      throw error;
+    }
+  }
+
+  private _updateTimeout(key: string) {
+    if (!this._expireTimeout) return;
+    clearTimeout(this._timeouts[key]);
+    this._timeouts[key] = setTimeout(() => this.expire(key), this._expireTimeout);
   }
 }
 
-async function cleanAsync(path: string) {
+function expireWithTrace(cache: Cache, key: string) {
   try {
-    await app.core.file.deleteAsync(path);
+    cache.expire(key);
+  } catch (error) {
+    app.core.error.trace(error);
+  }
+}
+
+async function removeWithTraceAsync(path: string) {
+  try {
+    await app.core.file.removeAsync(path);
   } catch (error) {
     app.core.error.trace(error);
   }
--- src/classes/Session.ts
+++ src/classes/Session.ts
@@ -1,22 +1,67 @@
 import * as app from '..';
 
-export class Session implements app.ISessionListItem {
+export class Session implements app.ISession {
+  private readonly _cache: app.Cache;
+  private readonly _id: string;
   private readonly _images: app.FutureMap<string>;
+  private readonly _isActive: app.Future<void>;
+  private readonly _url: string;
+  private _hasReject?: boolean;
+  private _pageCount?: number;
 
-  constructor(images: app.FutureMap<string>, pageCount: number, url: string) {
-    Object.defineProperty(this, '_images', {enumerable: false, writable: true});
-    this._images = images;
-    this.id = app.createUniqueId();
-    this.pageCount = pageCount;
-    this.url = url;
+  constructor(cache: app.Cache, id: string, url: string) {
+    this._cache = cache;
+    this._id = id;
+    this._images = new app.FutureMap();
+    this._isActive = new app.Future();
+    this._url = url;
+  }
+
+  expire(error?: Error) {
+    if (this._hasReject) return;
+    this._hasReject = true;
+    this._images.reject(error);
+    this._isActive.reject(error);
+    for (let i = 1; i <= (this._pageCount || 0); i++) this._cache.expire(`${this._id}/${i}`);
+  }
+
+  getData() {
+    const id = this._id;
+    const pageCount = this._pageCount || 0;
+    const url = this._url;
+    return {id, pageCount, url};
   }
 
   async getImageAsync(pageNumber: number) {
-    if (pageNumber < 0 || pageNumber > this.pageCount) return;
-    return await this._images.getAsync(String(pageNumber));
+    if (this._hasReject || pageNumber <= 0 || pageNumber > (this._pageCount || 0)) return;
+    const key = await this._images.getAsync(String(pageNumber));
+    const image = await this._cache.getAsync<string>(key);
+    return image;
   }
 
-  readonly id: string;
-  readonly pageCount: number;
-  readonly url: string;
+  get id() {
+    return this._id;
+  }
+
+  get isValid() {
+    return !this._hasReject && Boolean(this._pageCount);
+  }
+
+  async setImageAsync(pageNumber: number, image: string) {
+    if (this._hasReject || pageNumber <= 0 || pageNumber > (this._pageCount || 0)) return;
+    const key = `${this._id}/${pageNumber}`;
+    await this._cache.getOrAddAsync(key, () => image);
+    this._images.resolve(String(pageNumber), key);
+  }
+
+  setPageCount(pageCount: number) {
+    if (this._hasReject) return;
+    this._pageCount = this._pageCount || pageCount;
+    this._isActive.resolve();
+  }
+
+  async waitAsync() {
+    await this._isActive.getAsync();
+    return this.getData();
+  }
 }
