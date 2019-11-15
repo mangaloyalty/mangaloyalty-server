5c8de9c34c425cc9efb124144a024d5b93c9e8a1
Roel van Uden <r.h.g.v.uden@outlook.com>
2019-11-15 20:15:06 +0100

    Add `settings.json` for customized user settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,23 +1,26 @@
+import * as fs from 'fs-extra';
 import * as os from 'os';
 import * as path from 'path';
+const basePath = path.join(os.homedir(), 'mangaloyalty');
+const settingsPath = path.join(basePath, 'settings.json');
+const userSettings = fs.readJsonSync(settingsPath, {throws: false});
 
-export const settings = {
-  basePath: path.resolve(os.homedir(), 'mangaloyalty'),
-  browser: 'chrome',
-  browserHeadless: true,
-  browserExitTimeout: 60000,
-  browserNavigationTimeout: 30000,
-  browserUserData: 'user-data',
-  browserViewport: {width: 1920, height: 974},
-  cache: 'coreCache',
+export const settings = Object.assign({
+  cache: path.join(basePath, 'cache'),
   cacheDataTimeout: 600000,
-  cacheImageTimeout: 630000,
+  cacheImageTimeout: 1200000,
+  chrome: path.join(basePath, 'chrome'),
+  chromeHeadless: true,
+  chromeExitTimeout: 60000,
+  chromeNavigationTimeout: 30000,
+  chromeUserData: 'user-data',
+  chromeViewport: {width: 1920, height: 974},
   imageLibraryTimeout: 600000,
-  imageRemoteTimeout: 630000,
+  imageRemoteTimeout: 1200000,
   imageSessionTimeout: 600000,
-  library: 'library',
-  libraryAutomationInterval: 600000,
+  library: path.join(basePath, 'library'),
+  libraryAutomationTimeout: 600000,
   librarySeries: 'series',
   sessionTimeout: 600000,
-  sync: 'sync'
-};
+  sync: path.join(basePath, 'sync')
+}, userSettings);
