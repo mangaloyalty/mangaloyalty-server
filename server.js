bf9279cb238aa8d1f6e3c214096edb53155a8584
Roel van Uden <r.h.g.v.uden@outlook.com>
2020-04-29 19:18:54 +0200

    Export library; old as mangaloyalty-server/server

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
+const app = require('./dist');
 const bodyParser = require('body-parser');
 const compression = require('compression');
 const cors = require('cors');
 const express = require('express');
 const http = require('http');
-const launchApp = require('./dist/launch');
 const swaggerUi = require('swagger-ui-dist');
 
-// Initialize the server router.
+// Initialize the router.
 const router = express.Router();
 router.use(bodyParser.json());
 router.use(compression());
 router.use(cors());
 
-// Initialize the server router handlers.
+// Initialize the router handlers.
 router.get('/', (_, res) => res.redirect('/api/'));
 router.use('/api', express.static(`${__dirname}/public`));
 router.use('/api', express.static(swaggerUi.absolutePath()));
-router.use(launchApp.router);
+router.use(app.bootApp());
 
 // Initialize the server.
 if (require.main && require.main.filename.startsWith(__dirname)) {
   const serverApp = express();
   const server = http.createServer(serverApp);
-  launchApp.attachSocket(server);
+  app.attachSocket(server);
   serverApp.disable('x-powered-by');
   serverApp.use(router);
   server.listen(7783, () => console.log(`Server running on http://localhost:7783/`));
 } else {
-  module.exports = {attachSocket: launchApp.attachSocket, router};
+  module.exports = {...app, router};
 }
