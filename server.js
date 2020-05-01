03a149e13e7c0454ff52a52ff1fd2afb53d8ced9
Roel van Uden <r.h.g.v.uden@outlook.com>
2020-05-01 20:06:57 +0200

    Rewire `bootApp` as async using resource manager

--- server.js
+++ server.js
@@ -6,26 +6,30 @@ const express = require('express');
 const http = require('http');
 const swaggerUi = require('swagger-ui-dist');
 
-// Initialize the router.
-const router = express.Router();
-router.use(bodyParser.json());
-router.use(compression());
-router.use(cors());
+async function routerAsync() {
+  // Initialize the router.
+  const router = express.Router();
+  router.use(bodyParser.json());
+  router.use(compression());
+  router.use(cors());
 
-// Initialize the router handlers.
-router.get('/', (_, res) => res.redirect('/api/'));
-router.use('/api', express.static(`${__dirname}/public`));
-router.use('/api', express.static(swaggerUi.absolutePath()));
-router.use(app.bootApp());
+  // Initialize the router handlers.
+  router.get('/', (_, res) => res.redirect('/api/'));
+  router.use('/api', express.static(`${__dirname}/public`));
+  router.use('/api', express.static(swaggerUi.absolutePath()));
+  router.use(await app.bootAsync());
+  return router;
+}
 
-// Initialize the server.
 if (require.main && require.main.filename.startsWith(__dirname)) {
-  const serverApp = express();
-  const server = http.createServer(serverApp);
-  app.attachSocket(server);
-  serverApp.disable('x-powered-by');
-  serverApp.use(router);
-  server.listen(7783, () => console.log(`Server running on http://localhost:7783/`));
+  routerAsync().then((router) => {
+    const serverApp = express();
+    const server = http.createServer(serverApp);
+    app.attachSocket(server);
+    serverApp.disable('x-powered-by');
+    serverApp.use(router);
+    server.listen(7783, () => console.log(`Server running on http://localhost:7783/`));
+  });
 } else {
-  module.exports = {...app, router};
+  module.exports = {...app, routerAsync};
 }
